54de35d8eb9c8d5a85a2337de0fc4a99d852e18d
leeyngdo <lyd0531@kaist.ac.kr>
2024-12-30 15:54:47 +0000

    add color.py

---
+++ analysis/figure/color.py
@@ -0,0 +1,19 @@
+# Dictionary of {method: color} for easy theme switching
+METHOD_COLORS = {
+    "HyperSimba": "#C7DDEC", # blue
+    "Simba": "#FFDFC3",# orange
+    "TD7": "#F5C9C9", # red
+    "MR.Q": "#E4D9EE", # purple
+    "TD-MPC2": "#CAE7CA", # green
+    "DreamerV3": "#F8DDF0", # pink
+}
+
+# Dictionary of {method: color} for easy theme switching
+METHOD_BOUNDARY_COLORS = {
+    "HyperSimba": "#1F77B4", # blue
+    "Simba": "#FF7F0F",# orange
+    "TD7": "#D62729", # red
+    "MR.Q": "#9467BD", # purple
+    "TD-MPC2": "#2CA02C", # green
+    "DreamerV3": "#E377C2", # pink
+}
